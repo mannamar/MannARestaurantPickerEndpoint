01871f3a8a9860eb3bcb27d7c5e547c90ff5262c
Amardeep Mann <amar2slick@gmail.com>
2022-10-27 15:04:42 -0700

    Peer review copied

--- Controllers/RestaurantPickerController.cs
+++ Controllers/RestaurantPickerController.cs
@@ -2,7 +2,7 @@
 // 10-27-22
 // Restaurant Picker - Endpoint
 // Create a Restaurant Picker WebAPI project that takes in a cusine-type string variable via the URL and presents the user with a randomly generated restaurant to eat at of that type
-// Peer reviewed by
+// Peer Review by Marcel R.: Program ran perfectly, I liked how if you don't include a type of food in the url it'll return one from all three categories. Nice use of arrays and if statements, and the inclusion of .ToLower(). Awesome work! :D
 
 using System;
 using System.Collections.Generic;
